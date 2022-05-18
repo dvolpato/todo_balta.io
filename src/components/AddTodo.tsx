ca79ff541b2ebd355a7dd11f12c2d074d72a8256
Daniel Pereira Volpato <dvolpato@users.noreply.github.com>
2022-05-18 16:11:29 -0300

    Add / and /create routes (#21)

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddTodo() {
+  return (<h1>Novo Todo</h1>)
+}
+
+export default AddTodo;
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,14 +2,33 @@ import React from 'react';
 import TodoContext from '../contexts/TodoContext';
 import Navbar from './Navbar';
 import TodoList from './TodoList';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddTodo from './AddTodo';
+
+/* 
+ * - react-router-dom v6 replaced Switch with Routes 
+ * - Routes must be declared from more specific to more generic 
+ */
 
 function App() {
   return (
     <TodoContext>
-      <div className="uk-container">
+      <Router>
         <Navbar />
-        <TodoList />
-      </div>
+        <br />
+        <div className="uk-container">
+          <Routes>
+            <Route path="/create" element={<AddTodo />} />
+            <Route path="/" element={
+              <div>
+                <h4>Minha lista de tarefas</h4>
+                <TodoList />
+              </div>
+            }
+            />
+          </Routes>
+        </div>
+      </Router>
     </TodoContext>
   );
 }
